14cca42682da450c33da279d32139686b847ca9d
stcol316 <metagenesis@hotmail.co.uk>
2020-10-15 17:17:10 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text("Ask me anything"),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int answer = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              answer = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$answer.png'),
+        ),
+      ),
+    );
+  }
+}
